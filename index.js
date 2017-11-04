a9a0aaf88b459c663be4e3f56beb41c83d26e8fa
Ruben Buelvas <rcbuelvasv@gmail.com>
2017-11-04 11:54:44 -0500

    Info section created

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var map;
 function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.6860516, lng: -74.0000333},
-    zoom: 10
+    center: {lat: 40.7288542, lng: -73.9972302},
+    zoom: 15
   });
 }
+
+$('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+
+    return false;
+});
